6f4e6d139540e15c53ddeb3842bb51232dfa9568
toliner <toliner.rkt@gmail.com>
2019-12-18 10:53:05 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    application
+    id("org.openjfx.javafxplugin") version "0.0.8"
+}
+
+group = "dev.toliner"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.jfoenix:jfoenix:9.0.9")
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+javafx {
+    version = "11.0.2"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
